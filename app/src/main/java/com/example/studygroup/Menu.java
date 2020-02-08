97b38df917ee32271c5a370e1199e1d0504fbd6d
Ruxi <ruxifleseriu@gmail.com>
2020-02-08 17:32:39 +0000

    Merged login

--- app/src/main/java/com/example/studygroup/Menu.java
+++ app/src/main/java/com/example/studygroup/Menu.java
@@ -2,8 +2,6 @@ package com.example.studygroup;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
