218fb65bc747884949a8e027baa477823a666bb6
Radu Bonea <bonearadu@gmail.com>
2020-02-08 16:44:12 +0000

    Updated branch to current master status

--- app/src/main/java/com/example/studygroup/Menu.java
+++ app/src/main/java/com/example/studygroup/Menu.java
@@ -2,13 +2,8 @@ package com.example.studygroup;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
