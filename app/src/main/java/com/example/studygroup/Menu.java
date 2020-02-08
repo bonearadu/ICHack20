e736f72954ced473f7eae8fce8ff1ed25c990252
Codrin <cc1619@ic.ac.uk>
2020-02-08 16:14:53 +0000

    Updated Menu to have good names

--- app/src/main/java/com/example/studygroup/Menu.java
+++ app/src/main/java/com/example/studygroup/Menu.java
@@ -16,17 +16,8 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.menu);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
