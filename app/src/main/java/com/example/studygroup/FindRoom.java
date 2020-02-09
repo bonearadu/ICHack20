a664db9c609177a25c6a71e4afc731678ce5bf98
Radu Bonea <bonearadu@gmail.com>
2020-02-09 01:06:04 +0000

    Renamed Searchbar to FindRoom

--- app/src/main/java/com/example/studygroup/FindRoom.java
+++ app/src/main/java/com/example/studygroup/FindRoom.java
@@ -10,7 +10,7 @@ import android.widget.SearchView;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class SearchBar extends AppCompatActivity {
+public class FindRoom extends AppCompatActivity {
     SearchView mySearchView;
     ListView search_group;
     ArrayAdapter<String> adapter;
@@ -23,7 +23,7 @@ public class SearchBar extends AppCompatActivity {
         search_group = (ListView) findViewById(R.id.search_group);
         ArrayList<String> arrayGroup = new ArrayList<>();
         arrayGroup.addAll(Arrays.asList(getResources().getStringArray(R.array.my_groups)));
-        adapter = new ArrayAdapter<String>(SearchBar.this,
+        adapter = new ArrayAdapter<String>(FindRoom.this,
                 android.R.layout.simple_list_item_1, arrayGroup);
         search_group.setAdapter(adapter);
 
