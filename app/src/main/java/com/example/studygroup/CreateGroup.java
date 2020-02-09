a14471c3f574efefa5719db9ab3fa26deefce561
Ruxi <ruxifleseriu@gmail.com>
2020-02-09 05:38:01 +0000

    Front

--- app/src/main/java/com/example/studygroup/CreateGroup.java
+++ app/src/main/java/com/example/studygroup/CreateGroup.java
@@ -3,7 +3,6 @@ package com.example.studygroup;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 public class CreateGroup extends AppCompatActivity {
 
@@ -11,8 +10,6 @@ public class CreateGroup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
 }
