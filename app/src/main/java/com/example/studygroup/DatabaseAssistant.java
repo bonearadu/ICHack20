7aede73a6d7c873372b685da921a2c48bc390184
Sircoz <cuziacradu5@gmail.com>
2020-02-09 02:44:24 +0200

    databaseGod

--- app/src/main/java/com/example/studygroup/DatabaseAssistant.java
+++ app/src/main/java/com/example/studygroup/DatabaseAssistant.java
@@ -8,39 +8,54 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
+
+import static android.content.ContentValues.TAG;
 
 public class DatabaseAssistant {
 
     public final static FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-   public static void addObject(String str, Object obj){
+   public static void addObject(String str, Object obj, String objID){
         DatabaseReference myRef = database.getReference(str);
-        myRef.setValue(obj);
+        myRef.child(objID).setValue(obj);
     }
 
     public static void addStaticGroup(StudyGroup studyGroup){
-
+       addObject("studyGroups", studyGroup, "1");
+       //to change
     }
 
-    /*public static Stream<StudyGroup> getStudyGroups(){
+    public static List<StudyGroup> getStudyGroups(){
         DatabaseReference myRef = database.getReference("studyGroups");
+        final List<StudyGroup> result = new ArrayList<>();
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    result.add(postSnapshot.getValue(StudyGroup.class));
+                }
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
             }
-        });
-    }*/
+    });
+        return result;
+   }
+
+   public void deleteStudyGroup(String id){
+       DatabaseReference myRef = database.getReference("studyGroups").child(id);
+       myRef.removeValue();
+   }
+
+
+
+
 
 }
